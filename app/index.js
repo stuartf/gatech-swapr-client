f8bbfbc6aa816c41497890272ada82bd078f5149
Brad Reardon <brad.jay.reardon@gmail.com>
2018-02-15 12:26:01 -0500

    Various updates toward new build system using Parcel

---
+++ app/index.js
@@ -0,0 +1,24 @@
+import 'angular';
+import 'angular-local-storage';
+import 'angular-bootstrap';
+import 'angular-ui-router';
+import 'angular-youtube-mb';
+
+import './swapr.js';
+import './swapr.student.js';
+
+import './sidebar/SidebarCtrl.js';
+import './dashboard/DashboardCtrl.js';
+import './grades/GradesCtrl.js';
+import './grades/GradesDetailsCtrl.js';
+import './assignments/AssignmentsCtrl.js';
+import './assignments/PeerReviewCtrl.js';
+import './assignments/UploadCtrl.js';
+import './profile/ProfileCtrl.js';
+import './gradeVideo/GradeVideoCtrl.js';
+import './grades/GradesService.js';
+import './profile/ProfileService.js';
+import './assignments/YoutubeValidationService.js';
+import './assignments/AssignmentDB.js';
+
+import './styles/main.scss';
